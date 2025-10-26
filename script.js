a4d54215db05fe2070057dea18e41379e5561fa0
Garv Virmani <garvirmani77@gmail.com>
2025-10-26 19:54:53 +0530

    script

--- script.js
+++ script.js
@@ -52,3 +52,10 @@ function showToast(msg, color) {
   toast.classList.add("show");
   setTimeout(() => toast.classList.remove("show"), 3000);
 }
+
+  const toggle = document.querySelector('.menu-toggle');
+  const navLinks = document.querySelector('.nav-links');
+
+  toggle.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+  });
